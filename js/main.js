8ad71ebf34534eae49c87b836ab5d719bab204a8
Wayne Hofstra <wayne.hofstra@hva.nl>
2020-10-06 10:58:25 +0200

    Minor Change

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const ROW_AMOUNT = [ 20, 20 ];
+const ROW_AMOUNT  = [ 20, 20 ];
+const SNAKE_COLOR = 0x91fd01;
 
 var config = {
     type: Phaser.AUTO,
@@ -33,31 +34,15 @@ function preload ()
       altLeft: 'A',
       altRight: 'D'
   });
-
-  field = this.add.grid(config.width * 1/2, config.height * 1/2, config.width,
-     config.height, config.width / ROW_AMOUNT[0], config.height / ROW_AMOUNT[1],
-     0x09001a, 0x010101, 0x431d2b, 0x010101);
-
-  if (config.width >= config.height) {
-    field.width     = field.height;
-    field.cellWidth = field.cellHeight / ROW_AMOUNT[0] * ROW_AMOUNT[1];
-  }
-  else {
-    field.height     = field.width;
-    field.cellHeight = field.cellWidth / ROW_AMOUNT[1] * ROW_AMOUNT[0];
-  }
-
-  playerSegment = this.add.rectangle(config.width / 2, config.height / 2,
-    field.cellWidth, field.cellHeight, 0x91fd01);
 }
 
 function create ()
 {
-  //Background
-
+  ///Grid
+  InitGrid(this);
 
   //Player
-  //InitPlayer(this);
+  InitPlayer(this);
 
   //Items
   //InitApples(this);
@@ -74,9 +59,31 @@ function update ()
   KeyInput();
 }
 
+function InitGrid(scene)
+{
+  field = scene.add.grid(config.width * 1/2, config.height * 1/2, config.width,
+     config.height, config.width / ROW_AMOUNT[0], config.height / ROW_AMOUNT[1],
+     0x09001a, 0x010101, 0x431d2b, 0x010101);
+
+  if (config.width >= config.height) {
+    field.width     = field.height;
+    field.cellWidth = field.cellHeight / ROW_AMOUNT[0] * ROW_AMOUNT[1];
+  }
+  else {
+    field.height     = field.width;
+    field.cellHeight = field.cellWidth / ROW_AMOUNT[1] * ROW_AMOUNT[0];
+  }
+}
+
 function InitPlayer(scene)
 {
-  //player.setCollideWorldBounds(true);
+  player = scene.add.rectangle(field.cellWidth * (ROW_AMOUNT[0] - 1) * 1/2,
+    field.cellHeight * (ROW_AMOUNT[1] - 1) * (1/2), field.cellWidth, field.cellHeight, SNAKE_COLOR);
+}
+
+function AddPlayerSegment(position, direction)
+{
+
 }
 
 function InitApples(scene)
